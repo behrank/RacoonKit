267d0ab0901b55c54a5ace0da88c1ace6adf9d69
Behran Kankul <behrank@gmail.com>
2019-11-08 13:49:02 +0300

    fix.

--- RacoonKit/RacoonKit/Common.swift
+++ RacoonKit/RacoonKit/Common.swift
@@ -5,20 +5,8 @@
 //  Created by Behran Kankul on 15.05.2019.
 //
 
-import ImageIO
+import Foundation
 
-struct ImageHeaderData{
-    static var PNG: [UInt8] = [0x89]
-    static var JPEG: [UInt8] = [0xFF]
-    static var GIF: [UInt8] = [0x47]
-    static var TIFF_01: [UInt8] = [0x49]
-    static var TIFF_02: [UInt8] = [0x4D]
-}
-
-enum ImageFormat{
-    case Unknown, PNG, JPEG, GIF, TIFF
-}
-
-enum RacoonDimension {
+public enum RacoonDimension {
     case top(value: CGFloat = 0), left(value: CGFloat = 0), right(value: CGFloat = 0), bottom(value: CGFloat = 0)
 }
