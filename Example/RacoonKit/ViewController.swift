197c04ebdea63191f7d8fc787190f10fb2f943de
Behran Kankul <behrank@gmail.com>
2019-11-12 22:43:25 +0300

    tutorial update 2

--- Example/RacoonKit/ViewController.swift
+++ Example/RacoonKit/ViewController.swift
@@ -21,24 +21,19 @@ class ViewController: UIViewController {
         let fourthView = UIView()
         view.addSubviews(views: firstView, secondView, thirdView, fourthView)
         
+        //How to set margins to either self and other UIView subclasses
         firstView.setMargins(RacoonDimension.top(value: 8), RacoonDimension.left(value: 8), RacoonDimension.right(value: 8), RacoonDimension.bottom(value: 8))
         firstView.setMarginTo(view: secondView, of: RacoonDimension.top(value: 12))
+
+        //How to set dimensions
         firstView.setHeight(100)
         firstView.setWidth(125)
-        
         firstView.setWidthEqualToSuperview()
         firstView.setHeightEqualToSuperview()
-        
         firstView.setWidthEqualTo(view: fourthView)
         firstView.setHeightEqualTo(view: secondView)
         firstView.setWidthEqualTo(views: secondView, thirdView, fourthView)
         firstView.setHeightEqualTo(views: secondView, thirdView, fourthView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
