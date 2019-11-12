a09557eeabff6dc10e6d21e33e9f1837f7e28c7f
Behran Kankul <behrank@gmail.com>
2019-11-12 22:37:57 +0300

    tutorial update 1

--- Example/RacoonKit/ViewController.swift
+++ Example/RacoonKit/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import RacoonKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //How to add subviews.
+        let firstView = UIView()
+        let secondView = UIView()
+        let thirdView = UIView()
+        let fourthView = UIView()
+        view.addSubviews(views: firstView, secondView, thirdView, fourthView)
+        
+        firstView.setMargins(RacoonDimension.top(value: 8), RacoonDimension.left(value: 8), RacoonDimension.right(value: 8), RacoonDimension.bottom(value: 8))
+        firstView.setMarginTo(view: secondView, of: RacoonDimension.top(value: 12))
+        firstView.setHeight(100)
+        firstView.setWidth(125)
+        
+        firstView.setWidthEqualToSuperview()
+        firstView.setHeightEqualToSuperview()
+        
+        firstView.setWidthEqualTo(view: fourthView)
+        firstView.setHeightEqualTo(view: secondView)
+        firstView.setWidthEqualTo(views: secondView, thirdView, fourthView)
+        firstView.setHeightEqualTo(views: secondView, thirdView, fourthView)
     }
 
     override func didReceiveMemoryWarning() {
